3f7e477a15a42f5d0ae7085c110af54af7097b4a
Olivia Madson <124902143+hasholivia@users.noreply.github.com>
2023-02-13 12:24:05 -0300

    adding dependencies

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"github.com/gophermasters/x/handlers"
-	"net/http"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gophermasters/bug-free-report/server/routes"
 )
 
+type Server struct {
+	port   string
+	server *gin.Engine
+}
+
+func NewServer() Server {
+	return Server{
+		port:   "10000",
+		server: gin.Default(),
+	}
+}
+
+func (s *Server) run() {
+	router := routes.ConfigRoutes(s.server)
+
+	log.Printf("Server running at port: %v", s.port)
+	log.Fatal(router.Run(":" + s.port))
+}
 
 func main(){
 	http.HandleFunc("/", handlers.ServeHTML)
-	http.ListenAndServe(":10000", nil)
+	run()
 }
